793be2dcde9a1097ed71ee70ea299ada3f64069c
Ha Rim Ku <hrku19@gmail.com>
2020-05-06 13:48:05 -0700

    got rid of arbitrary login and home page

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 $(function() {
-    $("#accountButton").click(function(){
-        window.location = "account.html";
-    });
-
-
-    $("#submitCredential").click(function(e){
-        if($("#loginEmail").val() == "email@gmail.com" && ($("#loginPassword").val() == "1234")){
-            window.location = "home.html";
-        }
-        else {
-            alert("The email and password you entered don't match.");
-        }
-    });
-
     $("#signinButton").click(function(){
         $("#signinModal").modal('show');
     });
 
-   
 });
