354399c161a525f1b267080fd40d2f142927d1b2
Ha Rim Ku <hrku19@gmail.com>
2020-05-06 13:21:33 -0700

    arbitrary login & shopping cart content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio_project
+
+## This is an online marketplace project built with Bootstrap. 
--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@ $(function() {
         $("#signinModal").modal('show');
     });
 
-    
+
+    $("#submitCredential").click(function(e){
+        if($("#loginEmail").val() == "email@gmail.com" && ($("#loginPassword").val() == "1234")){
+            window.location = "home.html";
+        }
+        else {
+            alert("The email and password you entered don't match.");
+        }
+    });
+
+   
 });
